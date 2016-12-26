3db80d455d2a890e7b94645e4db921816fb5e3f9
Jordannoel <jordannoel1@live.fr>
2016-12-26 16:47:24 +0100

    découpe du code dans main et client

--- main.py
+++ main.py
@@ -1,34 +1,34 @@
 #!/usr/bin/python3
 
 from grid import *
-import  random
+import random
+import socket
+import select
+
+HOST = ''
+PORT = 7777
 
 def main():
-    grids = [grid(), grid(), grid()]
-    current_player = J1
-    grids[J1].display()
-    while grids[0].gameOver() == -1:
-        if current_player == J1:
-            shot = -1
-            while shot <0 or shot >=NB_CELLS:
-                shot = int(input ("quel case allez-vous jouer ?"))
-        else:
-            shot = random.randint(0,8)
-            while grids[current_player].cells[shot] != EMPTY:
-                shot = random.randint(0,8)
-        if (grids[0].cells[shot] != EMPTY):
-            grids[current_player].cells[shot] = grids[0].cells[shot]
-        else:
-            grids[current_player].cells[shot] = current_player
-            grids[0].play(current_player, shot)
-            current_player = current_player%2+1
-        if current_player == J1:
-            grids[J1].display()
-    print("game over")
-    grids[0].display()
-    if grids[0].gameOver() == J1:
-        print("You win !")
-    else:
-        print("you loose !")
-
+	
+	s = socket.socket(family = socket.AF_INET6, type = socket.SOCK_STREAM, proto = 0, fileno = None) #création de la socket serveur
+	s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) #pour relancer rapidement la connexion sur le même port
+	s.bind((HOST, PORT)) #on associe au serveur le port 7777
+	s.listen(1) #le serveur va accepter des connexions
+	liste = [s] #liste contient tous les clients à qui envoyer
+	
+	while True:
+		socketLecture,_,_ = select.select(liste, [], []) 	#socketLecture contient toutes les sockets disponibles en lecture
+		for n in socketLecture: 							#on parcourt la liste des sockets 
+			if n == s: 		#si n est la socket d'écoute (çàd un client vient de se connecter)
+				connex, _ = s.accept() #connex contient l'adresse du client
+				liste.append(connex) #que l'on ajoute à la liste des clients
+			else:			
+				data = n.recv(1500)
+				if not data: 
+					n.close()
+					liste.remove(n)
+					continue 	#siginifie arrête toi là et passe à l'itération suivante
+				for i in liste:
+					if i!=s: 	#si la socket est différente de celle qui nous a envoyé un message
+						i.sendall(data) #on envoie à toutes les autres socket le message envoyé par s
 main()
