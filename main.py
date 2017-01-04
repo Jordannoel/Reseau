ca71efb98d7ab7d9cb3b0834aea731f887ec247d
Jordannoel <jordannoel1@live.fr>
2017-01-04 19:26:17 +0100

    tout le code dans le main
    les différents clients connaissent leur n° de joueur

--- main.py
+++ main.py
@@ -4,31 +4,106 @@ from grid import *
 import random
 import socket
 import select
+import sys
+import time
 
 HOST = ''
 PORT = 7777
 
 def main():
 	
-	s = socket.socket(family = socket.AF_INET6, type = socket.SOCK_STREAM, proto = 0, fileno = None) #création de la socket serveur
-	s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) #pour relancer rapidement la connexion sur le même port
-	s.bind((HOST, PORT)) #on associe au serveur le port 7777
-	s.listen(1) #le serveur va accepter des connexions
-	liste = [s] #liste contient tous les clients à qui envoyer
+	if len(sys.argv) == 1:
+
+		s = socket.socket(socket.AF_INET6, socket.SOCK_STREAM, 0)
+		s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+		s.bind((HOST, PORT))
+		s.listen(1)
+		liste=[]		#liste contient tous les clients, même le serveur (je crois)
+		while True: 
+			socketList,_,_ = select.select(liste+[s], [], []) 	#contient la liste des sockets en lecture
+			for n in socketList: 							#on parcourt la liste des sockets 
+				if (n == s): 		#si n est la socket d'écoute (çàd un client vient de se connecter)
+					connex, _ = s.accept() #on créé une nouvelle connexion entre le client et le serveur
+					liste.append(connex) #que l'on ajoute à la liste des clients
+					if len(liste) == 1:
+						connex.send("1er joueur".encode())
+					if len(liste) == 2:
+						connex.send("2ème joueur".encode())
+					if len(liste) > 2:
+						connex.send("Spectateur".encode())
+				else:			
+					data = n.recv(1500)
+					if(data==0): 
+						n.close()
+						liste.remove(n)
+						break #siginifie arrête toi là et passe à l'itération suivante
+					for i in liste:
+						if (i!=n): 	#si la socket est différente de celle qui nous a envoyé un message
+							i.send(data) #on envoie à toutes les autres socket le message envoyé par s
+
+
+
+
+	elif len(sys.argv) == 2:
 	
-	while True:
-		socketLecture,_,_ = select.select(liste, [], []) 	#socketLecture contient toutes les sockets disponibles en lecture
-		for n in socketLecture: 							#on parcourt la liste des sockets 
-			if n == s: 		#si n est la socket d'écoute (çàd un client vient de se connecter)
-				connex, _ = s.accept() #connex contient l'adresse du client
-				liste.append(connex) #que l'on ajoute à la liste des clients
-			else:			
-				data = n.recv(1500)
-				if not data: 
-					n.close()
-					liste.remove(n)
-					continue 	#siginifie arrête toi là et passe à l'itération suivante
-				for i in liste:
-					if i!=s: 	#si la socket est différente de celle qui nous a envoyé un message
-						i.sendall(data) #on envoie à toutes les autres socket le message envoyé par s
+		s = socket.socket(family = socket.AF_INET6, type = socket.SOCK_STREAM, proto = 0, fileno = None) #création de la socket
+		s.connect((HOST, 7777))
+		string_player = s.recv(1500).decode()
+		if string_player == "1er joueur":
+			current_player = J1
+		elif string_player == "2ème joueur":
+			current_player = J2
+		elif string_player == "Spectateur":
+			current_player = 0
+		print(current_player)
+		
+		while True:
+			
+			grids = [grid(), grid(), grid()]
+			grids[current_player].display()
+			while grids[0].gameOver() == -1:
+				if current_player == J1 or J2:
+					shot = -1
+					while shot <0 or shot >=NB_CELLS:
+						shot = int(input ("Quelle case allez-vous jouer ?"))
+					if (grids[0].cells[shot] != EMPTY):
+						grids[current_player].cells[shot] = grids[0].cells[shot]
+					else:
+						grids[current_player].cells[shot] = current_player
+						grids[0].play(current_player, shot)
+						current_player = current_player%2+1
+					
+					s.send(bytes(shot))
+					grids[current_player].display()
+				else:
+					shot = s.recv(1500)
+					
+					
+
+			print("Game over")
+			grids[0].display()
+			if grids[0].gameOver() == J1:
+				print("You win !")
+			else:
+				print("You lose !")
+				
+			"""------- permet au joueur de rejouer tant qu'il veut-------"""
+			answer = ''
+			while True: 
+				answer = input("Do you want to continue? y/n ")
+				print(answer)
+				if answer != 'y' or answer != 'n':
+					break
+			if answer == 'n':
+				break
+			"""----------------------------------------------------------"""
+		#fin du while(True)
+	#fin du client
+							
 main()
+
+
+#else:
+				#	shot = random.randint(0,8)
+				#	while grids[current_player].cells[shot] != EMPTY:
+				#		shot = random.randint(0,8)
